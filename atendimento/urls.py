cc9cf2132b16d4ec480cc858d120356d839336f0
lelanera <leandrolanera@gmail.com>
2021-05-08 15:00:44 -0300

    Tela de detalhes dos projetos

--- atendimento/urls.py
+++ atendimento/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, lista_projetos
+from .views import home, lista_projetos, detalhe_projeto
 
 urlpatterns = [
     path('', home, name='home'),
     path('projetos', lista_projetos, name='lista-projetos'),
+    path('detalhes/<int:id>/', detalhe_projeto, name='detalhe_projeto'),
 ]
--- atendimento/views.py
+++ atendimento/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Projects
 from .forms import ProjectsForm
 from django.db import connections
+from django.db import connection
 
 
 def home(request):
@@ -16,7 +17,7 @@ def dictfetchall(cursor):
     ]
 
 def lista_projetos(request):
-    sql = "select distinct (A.name) as projeto," 
+    sql = "select distinct A.id, (A.name) as projeto," 
     sql = sql +  " count(B.id) as demandas,"
     sql = sql +  " (select max(CC.name) from redmine.issues BB inner join redmine.versions CC ON (BB.fixed_version_id = CC.id) "
     sql = sql +  " where BB.project_id = A.id and BB.tracker_id = 23) as ultimaversao"
@@ -34,3 +35,32 @@ def lista_projetos(request):
         SasProjetos = dictfetchall(cursor)
     
     return render(request, 'lista-projetos.html',{'projects':SasProjetos})
+
+def detalhe_projeto(request, id):
+    
+    sql = "select A.name, "
+    sql = sql + " (select max(CC.name) from redmine.issues BB inner join redmine.versions CC ON (BB.fixed_version_id = CC.id)"
+    sql = sql + "             where BB.project_id = A.id and BB.tracker_id = 23) as ultimaversao "
+    sql = sql + " from redmine.projects A where id = % s" % id
+    
+    with connections['redminedb'].cursor() as cursor:
+        cursor.execute(sql)
+        ProjetoPrinc = dictfetchall(cursor)
+
+    sql = " select distinct C.name as versao,"
+    sql = sql + " (A.name) as nome,"
+    sql = sql + " count(B.id) as demandas,"
+    sql = sql + " count(IF(B.closed_on is null,1,null)) as demabertas,"
+    sql = sql + " count(IF(B.tracker_id=23,1,null)) as temaceite"
+    sql = sql + " from redmine.projects A"
+    sql = sql + " inner join redmine.issues B ON (A.id = B.project_id)"
+    sql = sql + " inner join redmine.versions C ON (B.fixed_version_id = C.id)"
+    sql = sql + " where A.id = % s" % id
+    sql = sql + " group by C.name"
+    sql = sql + " order by  C.name desc"
+
+    with connections['redminedb'].cursor() as cursor:
+        cursor.execute(sql)
+        DetProjeto = dictfetchall(cursor)
+
+    return render(request, 'detalhes-projeto.html',{'detalhesProjeto':DetProjeto, 'projetoPrinc':ProjetoPrinc})
